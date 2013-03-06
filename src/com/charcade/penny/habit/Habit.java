b336c5695ae589e53f6ea11d8b1fb2d3899bb102
Vin Gardner <vcrgardner@gmail.com>
2013-03-06 23:04:28 +0000

    Creates Habit class.

---
+++ src/com/charcade/penny/habit/Habit.java
@@ -0,0 +1,75 @@
+package com.charcade.penny.habit;
+
+import java.math.BigDecimal;
+import java.security.Timestamp;
+
+import android.R.bool;
+
+public class Habit {
+	private int id;
+	private String name;
+	private BigDecimal value;
+	private int weight;
+	private Boolean visible;
+	private Timestamp created;
+	private Timestamp updated;
+	
+	public Habit(){
+		
+	}
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public BigDecimal getValue() {
+		return value;
+	}
+
+	public void setValue(BigDecimal value) {
+		this.value = value;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public Boolean getVisible() {
+		return visible;
+	}
+
+	public void setVisible(Boolean visible) {
+		this.visible = visible;
+	}
+
+	public Timestamp getCreated() {
+		return created;
+	}
+
+	public void setCreated(Timestamp created) {
+		this.created = created;
+	}
+
+	public Timestamp getUpdated() {
+		return updated;
+	}
+
+	public void setUpdated(Timestamp updated) {
+		this.updated = updated;
+	}
+}
