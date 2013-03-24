0272bfb03e5984f1f81ccefd99513b51c5bf545c
Vin Gardner <vcrgardner@gmail.com>
2013-03-24 21:22:02 +0000

    Creating Transaction class.

--- src/com/charcade/penny/entities/Transaction.java
+++ src/com/charcade/penny/entities/Transaction.java
@@ -1,5 +1,42 @@
 package com.charcade.penny.entities;
 
-public class Transaction {
+import java.math.BigDecimal;
+import java.security.Timestamp;
 
+public class Transaction {
+	private int tid;
+	private int hid;
+	private int gid;
+	private BigDecimal value;
+	private Timestamp created;
+	public int getTid() {
+		return tid;
+	}
+	public void setTid(int tid) {
+		this.tid = tid;
+	}
+	public int getHid() {
+		return hid;
+	}
+	public void setHid(int hid) {
+		this.hid = hid;
+	}
+	public int getGid() {
+		return gid;
+	}
+	public void setGid(int gid) {
+		this.gid = gid;
+	}
+	public BigDecimal getValue() {
+		return value;
+	}
+	public void setValue(BigDecimal value) {
+		this.value = value;
+	}
+	public Timestamp getCreated() {
+		return created;
+	}
+	public void setCreated(Timestamp created) {
+		this.created = created;
+	}
 }
