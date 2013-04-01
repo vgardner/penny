a9f52e75d4f57dde83a5f0a8d5374d7a5ca483a8
Vin Gardner <vcrgardner@gmail.com>
2013-04-01 23:11:36 +0100

    Updates GoalDbMap.

--- src/com/charcade/penny/db/GoalDbMap.java
+++ src/com/charcade/penny/db/GoalDbMap.java
@@ -6,8 +6,8 @@ import java.security.Timestamp;
 import android.provider.BaseColumns;
 
 public abstract class GoalDbMap implements BaseColumns {
-	public static final String TABLE_NAME = "habit";
-	public static final String COLUMN_NAME_HID = "hid";
+	public static final String TABLE_NAME = "goal";
+	public static final String COLUMN_NAME_GID = "gid";
 	public static final String COLUMN_NAME_NAME = "name";
 	public static final String COLUMN_NAME_DECRIPTION = "description";
 	public static final String COLUMN_NAME_VALUE = "value";
@@ -16,13 +16,15 @@ public abstract class GoalDbMap implements BaseColumns {
 	public static final String COLUMN_NAME_CREATED = "created";
 	public static final String COLUMN_NAME_UPDATED = "updated";
 	public static final String COLUMN_NAME_ICON = "icon";
+	public static final String COLUMN_NAME_DEADLINE = "deadline";
+	public static final String COLUMN_NAME_TOTAL_SAVED = "totalsaved";
 	
 	private static final String TEXT_TYPE = " TEXT";
 	private static final String AUTOINCREMENT_TYPE = " INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL";
 	private static final String COMMA_SEP = ",";
 	static final String SQL_CREATE =
 	    "CREATE TABLE " + TABLE_NAME + " (" +
-	    COLUMN_NAME_HID + AUTOINCREMENT_TYPE + COMMA_SEP +
+	    COLUMN_NAME_GID + AUTOINCREMENT_TYPE + COMMA_SEP +
 	    COLUMN_NAME_NAME + TEXT_TYPE + COMMA_SEP +
 	    COLUMN_NAME_VALUE + TEXT_TYPE +
 	    /*COLUMN_NAME_NAME + TEXT_TYPE + COMMA_SEP +
